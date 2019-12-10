1e9ae4f51d1a4b694708408e3c09dc31845ceebc
Takayoshi Aoyagi <takayoshi_aoyagi@waku-2.com>
2019-12-10 16:35:20 +0900

    beautify

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
 Vue.component('piston-view', {
     props: ['index', 'state'],
     template: `
-      <span v-if="state===true">{{index}}</span>
-      <span v-else>-</span>
+      <span v-if="state===true" class="piston-on">{{index}}</span>
+      <span v-else class="piston-off">-</span>
 `
 });
 
 Vue.component('pistons-view', {
     props: ['item', 'base'],
     template: `
-      <span>
-          <span v-if="base === 'C'">
-              {{item.realNames[1] || item.realNames[0]}} {{item.pitch}}:
-          </span>
-          <span v-else>
-              {{item.names[1] || item.names[0]}} {{item.pitch}}:
+      <div>
+          <span class="note">
+              <span v-if="base === 'C'">
+                  <span>{{item.realNames[1] || item.realNames[0]}}</span>
+                  <span>{{item.pitch}}</span>
+              </span>
+              <span v-else>
+                  <span>{{item.names[1] || item.names[0]}}</span>
+                  <span> {{item.pitch}}</span>
+              </span>
           </span>
           <piston-view :index="1" :state="item.pistons[0]"></piston-view>
           <piston-view :index="2" :state="item.pistons[1]"></piston-view>
           <piston-view :index="3" :state="item.pistons[2]"></piston-view>
-      </span>
+      </div>
 `
 });
 
@@ -47,6 +51,17 @@ const app =  new Vue({
     methods: {
 	getItems: function () {
 	    this.items = Scales.get(this.scaleType, this.tonalCenter, this.base);
+	}
+    },
+    watch: {
+	scaleType: function () {
+	    this.getItems();
+	},
+	tonalCenter: function () {
+	    this.getItems();
+	},
+	base: function () {
+	    this.getItems();
 	}
     }
 });
